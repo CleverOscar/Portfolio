ab97f5ad86897fcd3285018b001f9c241d377d2e
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-08-05 21:35:38 -0500

    Home Page Started
    Home page infomation started, need to add more styles and description

--- src/views/home_component/home_page.js
+++ src/views/home_component/home_page.js
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
 import './home.css'
+import {Container, Row, Col} from 'reactstrap'
 
 function HomePage(){
   return (
     <div className="home_container">
-      <h1>Home Page</h1>
+      <Container>
+        <div>
+          <h1>Full Stack Developer & Software Engineer</h1>
+        </div>
+        <Row>
+        <Col>
+          <img src="https://media-exp1.licdn.com/dms/image/C5103AQHV3EbEhHOjaw/profile-displayphoto-shrink_200_200/0?e=1602115200&v=beta&t=XDtIKMn0jhBC7G3b6oFFlYCnkCcKJu-ps1xEwjEb_0s" />
+        </Col>
+        <Col>
+           Hello, my name is Oscar Ortiz!
+          And I am a Full - Stack Web Developer and Software Engineer.
+          Sounds fancy but with all honesty, I still have much to learn,
+          I am very eager to
+          get started on everything else.
+        </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
