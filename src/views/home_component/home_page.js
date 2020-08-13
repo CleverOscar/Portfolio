5594c47371c63d424aa1d2e766ad1b1b5b75254c
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-08-13 16:08:32 -0500

    Update home_page.js
    home_page updated for responsive

--- src/views/home_component/home_page.js
+++ src/views/home_component/home_page.js
@@ -8,13 +8,13 @@ function HomePage(){
       <Container className="home_container">
           <h1>Full Stack Developer & Software Engineer</h1>
         <Row className="quick_bio">
-        <Col className="dummy-Container">
+        <Col className="dummy-Container" fluid="md">
           <img src="https://media-exp1.licdn.com/dms/image/C5103AQHV3EbEhHOjaw/profile-displayphoto-shrink_200_200/0?e=1602115200&v=beta&t=XDtIKMn0jhBC7G3b6oFFlYCnkCcKJu-ps1xEwjEb_0s" />
         </Col>
-        <Col className="dummy-Container">
-           <h3>Hello, my name is Oscar Ortiz!
-               And I am a Full - Stack Web Developer
-               and Software Engineer.</h3>
+        <Col className="dummy-Container" fluid="md">
+          <h3>
+            Hello, my name is Oscar Ortiz!
+          </h3>
         </Col>
         </Row>
       </Container>
