eedd6287cd39cfa05a963f41fe05fdb36888e558
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-08-23 15:56:29 -0500

    Style Changes

--- src/views/home_component/home_page.js
+++ src/views/home_component/home_page.js
@@ -1,30 +1,17 @@
 import React, { useState } from 'react';
 import './home.css'
 import {Container, Row, Col} from 'reactstrap'
+import Projects from '../projects/projects.js';
+import AboutMe from '../minor_components/about.js';
 
 function HomePage(){
   return (
     <div>
       <Container className="home_container">
-          <h1>Full Stack Developer & Software Engineer</h1>
-        <Row className="quick_bio">
-        <Col className="dummy-Container" fluid="md">
-          <img src="https://media-exp1.licdn.com/dms/image/C5103AQHV3EbEhHOjaw/profile-displayphoto-shrink_200_200/0?e=1602115200&v=beta&t=XDtIKMn0jhBC7G3b6oFFlYCnkCcKJu-ps1xEwjEb_0s" />
-        </Col>
-        <Col className="dummy-Container" fluid="md">
-          <h3>
-            Hello, my name is Oscar Ortiz!
-          </h3>
-        </Col>
-        </Row>
+        
+        <AboutMe />
 
-        <Row>
-          <Col className='projects-container'>
-            <h2>
-              Projects
-            </h2>
-          </Col>
-        </Row>
+        <Projects/>
       </Container>
     </div>
   )
---
+++ src/views/projects/projects.js
@@ -0,0 +1,12 @@
+import React, { useState, ReactComponent } from 'react'
+import { render } from '@testing-library/react'
+
+function Projects(){
+    return (
+        <div> 
+            Projects page
+        </div>
+    )
+}
+
+export default Projects;
