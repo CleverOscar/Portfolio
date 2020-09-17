ec91e9c447236a5c4aed938fa14b5a449bcce181
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-09-17 17:25:32 -0500

    image size fixed
    Image sizes need to be fixed to fit mobile device.

--- src/views/projects/projects.js
+++ src/views/projects/projects.js
@@ -8,11 +8,15 @@ function Projects(){
             <Row xs='1' sm='1' md='3'>
                 <Col> 
                     <h4> Tinder Carousel Guide  </h4>
-                    < img className="gallary" src="https://i.imgur.com/Pg6091Z.png" alt=" Tinder Carousel Guide / Learn to build a Tinder like Carousel" /> 
+                    < img className = "gallary"
+                    src = "https://i.imgur.com/GzeszOR.png"
+                    alt = " Tinder Carousel Guide / Learn to build a Tinder like Carousel" / >
                 </Col>
                 <Col>
                     <h4> Video Player Guide </h4>
-                    < img className="gallary" src="https://i.imgur.com/4Mt3r7l.png" alt=" Video Player Guide / Learn to build your own custom video player" />
+                    < img className = "gallary"
+                    src = "https://i.imgur.com/sVvzWgr.png"
+                    alt = " Video Player Guide / Learn to build your own custom video player" / >
                 </Col>
                 <Col>
                     <h4> Neighborhood Chef </h4>
