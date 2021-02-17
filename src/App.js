b7a23aa1ee5b587d0e6024ad3ebc157734be27a2
Oscar Pacheco <Oscarpa88@yahoo.com>
2021-02-16 18:11:36 -0600

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React from 'react';
 import {Container} from 'reactstrap';
 
 // Views
-
 // Navbar
-
 import NavbarMenu from './navbar/Navbar.js';
 
 // Components
@@ -13,21 +11,47 @@ import About from './views/About/About.js';
 import CarouselComponent from './views/Carousel/Carousel.js';
 
 
-function App() {
-return (
-  <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 1,
+          src: 'https://dummyimage.com/hd720',
+          altText: 'Slide 1',
+          caption: 'Slide 1'
+        }, {
+          id: 2,
+          src: 'https://dummyimage.com/hd720',
+          altText: 'Slide 2',
+          caption: 'Slide 2'
+        }, {
+          id: 3,
+          src: 'https://dummyimage.com/hd720',
+          altText: 'Slide 3',
+          caption: 'Slide 3'
+        }
+      ],  
+    }
+  }
+  
+  render() {
+    return (
+    <div className="App">
 
-    <div>
-      <NavbarMenu />
+      <div>
+        <NavbarMenu />
+      </div>
+
+      <Container>
+        < About />
+        
+        <CarouselComponent carouselProps={this.state.items} />
+      </Container>
     </div>
-
-    <Container>
-      < About />
-      
-      <CarouselComponent />
-    </Container>
-  </div>
-);
+  )
+  }
 }
 
 export default App;
