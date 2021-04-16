0266db45474eb3f8b06afabc9241cbacd9d0ef10
Oscar Pacheco <Oscarpa88@yahoo.com>
2021-04-16 17:03:32 -0500

    Update App.js
    Projects Are displaying on home page now

--- src/App.js
+++ src/App.js
@@ -66,6 +66,7 @@ class App extends React.Component {
           <Route exact path="/">
               <h1 className="title">Full-Stack Web Developer</h1>
               <About />
+              <ProjectsPage images={this.state.images} />
           </Route>
         </Switch>
       </Container>
