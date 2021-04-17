3eb1327200682d9bc6676daac566255948fce72f
Oscar Pacheco <Oscarpa88@yahoo.com>
2021-04-17 14:30:52 -0500

    Footer
    Footer displaying on homepage

--- src/views/Footer/Footer.js
+++ src/views/Footer/Footer.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import './Footer.css';
 
 
 const Footer = (props) => (
     <div className='footer_component'>
-            <p> Footer </p>
+            <div className="footer-titles">
+                <h5>Social</h5>
+                <h5>Links</h5>
+            </div>
+
+            
+                <div className="links">
+                    <ul>
+                        <li>
+                            LinkedIn
+                        </li>
+                        <li>
+                            Twitter
+                        </li>
+                        <li>
+                            GitHub
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="links">
+                    <ul>
+                        <li>
+                            Home
+                        </li>
+                        <li>
+                            About
+                        </li>
+                        <li>
+                            Projects
+                        </li>
+                    </ul>
+                </div>
+            
     </div>
 )
 
