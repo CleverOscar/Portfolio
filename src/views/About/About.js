e6c78851cc49d90ed987ade743c01b6ee6daada0
Oscar Pacheco <Oscarpa9188@gmail.com>
2021-04-19 19:28:40 -0500

    About Component
    About Component now responsive on multiple screens

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -9,11 +9,8 @@ const About = () => {
     return (<div className="about-container">   
        <Row>
            <Col xs="auto" sm="auto" md="auto" >
-             < img src = {
-                 'https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/35686839_2000045286694293_6539718802064539648_o.jpg?_nc_cat=105&ccb=1-3&_nc_sid=174925&_nc_ohc=MWS178PdUCwAX_KwzfG&_nc_ht=scontent-ort2-2.xx&oh=71b061bf46af60053af18ad8beeb98bd&oe=607DA1AF'
-             }
-             alt = 'Oscar Self Portriat' /
-                 >
+             <img src={'https://i.ibb.co/SnDDJ99/45219372-2219308534767966-7383583342043594752-n.jpg'}
+             alt='Oscar Self Portriat'/>
            </Col>
            <Col sm="auto" md="auto">
                 <h3>
