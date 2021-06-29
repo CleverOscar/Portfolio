1ac0b5fc3876126398a2d8cc9ecf4093c470e822
Oscar Pacheco <Oscarpa9188@gmail.com>
2021-06-29 18:10:16 -0500

    Update Contact.js

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -1,40 +1,58 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import {Col, Form, Button, FormGroup, Input} from 'reactstrap';
+import { Col, Form, Button, FormGroup, Input } from 'reactstrap';
 import './contact.css';
 
 
 class ContactForm extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+
+        }
     }
 
-    render(){
-           return( 
-            <div className="contact_form">
-                <h3 className="contact_title">Contact Form</h3> 
-                <Form>
-                    <FormGroup>
-                        <Col>
-                            <Input type="text" placeholder="Full Name" />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup>
-                    <Col>
-                    <Input type="email" placeholder="Email" />
-                    </Col>        
-                    </FormGroup>
-                    <FormGroup>
-                    <Col>
-                    <Input type="textarea" name="text" id="exampleText" />
-                    </Col>
-                    </FormGroup>
-                    <Col>
-                    <Button>Submit</Button>
-                    </Col>
-                </Form>
-            </div>
+    render() {
+        return ( <
+            div className = "contact_form" >
+            <
+            h3 className = "contact_title" > Contact Form < /h3>  <
+            Form >
+            <
+            FormGroup >
+            <
+            Col >
+            <
+            Input type = "text"
+            placeholder = "Full Name" / >
+            <
+            /Col> < /
+            FormGroup > <
+            FormGroup >
+            <
+            Col >
+            <
+            Input type = "email"
+            placeholder = "Email" / >
+            <
+            /Col>         < /
+            FormGroup > <
+            FormGroup >
+            <
+            Col >
+            <
+            Input type = "textarea"
+            name = "text"
+            id = "exampleText" / >
+            <
+            /Col> < /
+            FormGroup > <
+            Col >
+            <
+            Button > Submit < /Button> < /
+            Col > <
+            /Form> < /
+            div >
         )
     }
 }
