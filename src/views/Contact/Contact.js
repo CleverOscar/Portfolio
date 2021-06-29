1af3be46b5b22dc8c9b4d77c8ff8160e15cc178c
Oscar Pacheco <Oscarpa9188@gmail.com>
2021-06-29 18:08:22 -0500

    Updated Form

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -1,33 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import {Col, Form, Button, FormGroup, Input} from 'reactstrap';
 import './contact.css';
 
 
-const ContactForm =  (props) => (
-    <div className="contact_form">
-        <h3 className="contact_title">Contact Form</h3> 
-        <Form>
-        <FormGroup>
-            <Col>
-                <Input type="text" placeholder="Full Name" />
-            </Col>
-        </FormGroup>
-        <FormGroup>
-            <Col>
-                <Input type="email" placeholder="Email" />
-            </Col>        
-        </FormGroup>
-        <FormGroup>
-            <Col>
-                <Input type="textarea" name="text" id="exampleText" />
-            </Col>
-        </FormGroup>
-        <Col>
-            <Button>Submit</Button>
-        </Col>
-        </Form>
-    </div>
-)
+class ContactForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
+
+    render(){
+           return( 
+            <div className="contact_form">
+                <h3 className="contact_title">Contact Form</h3> 
+                <Form>
+                    <FormGroup>
+                        <Col>
+                            <Input type="text" placeholder="Full Name" />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup>
+                    <Col>
+                    <Input type="email" placeholder="Email" />
+                    </Col>        
+                    </FormGroup>
+                    <FormGroup>
+                    <Col>
+                    <Input type="textarea" name="text" id="exampleText" />
+                    </Col>
+                    </FormGroup>
+                    <Col>
+                    <Button>Submit</Button>
+                    </Col>
+                </Form>
+            </div>
+        )
+    }
+}
 
 export default ContactForm;
