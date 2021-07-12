40e4dc80ccafbf44b3f9fb68dd3556329fca5342
Oscar Pacheco Ortiz <Oscarpa9188@gmail.com>
2021-07-12 11:20:33 -0500

    Projects page (#30)
    * testing-production
    
    * Production testing
    
    * Create _redirects
    
    * Updated
    
    * Updated Changes
    
    * Update Footer.js
    
    * Update ImageList.js
    
    * Update Navbar.js
    
    * Updated Form
    
    * Update Contact.js
    
    * Psuh

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -14,46 +14,12 @@ class ContactForm extends Component {
     }
 
     render() {
-        return ( <
-            div className = "contact_form" >
-            <
-            h3 className = "contact_title" > Contact Form < /h3>  <
-            Form >
-            <
-            FormGroup >
-            <
-            Col >
-            <
-            Input type = "text"
-            placeholder = "Full Name" / >
-            <
-            /Col> < /
-            FormGroup > <
-            FormGroup >
-            <
-            Col >
-            <
-            Input type = "email"
-            placeholder = "Email" / >
-            <
-            /Col>         < /
-            FormGroup > <
-            FormGroup >
-            <
-            Col >
-            <
-            Input type = "textarea"
-            name = "text"
-            id = "exampleText" / >
-            <
-            /Col> < /
-            FormGroup > <
-            Col >
-            <
-            Button > Submit < /Button> < /
-            Col > <
-            /Form> < /
-            div >
+        return ( 
+            <div>
+                <Col>
+                    Form 
+                </Col>
+            </div>
         )
     }
 }
